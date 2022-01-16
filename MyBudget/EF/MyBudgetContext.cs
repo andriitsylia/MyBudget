f78187f478b5374b55a88775c31d5b1b921a3b20
Andrii Tsylia <andrii.tsylia@outlook.com>
2022-01-16 15:57:09 +0400

    Add migration.

--- MyBudget/EF/MyBudgetContext.cs
+++ MyBudget/EF/MyBudgetContext.cs
@@ -9,7 +9,7 @@ namespace MyBudget.EF
         {
 
         }
-        public DbSet<IncomeType> IncomeTypes { get; set }
+        public DbSet<IncomeType> IncomeTypes { get; set; }
         public DbSet<Income> Incomes { get; set; }
         public DbSet<ExpenseType> ExpenseTypes { get; set; }
         public DbSet<Expense> Expenses { get; set; }
